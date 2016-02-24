570210712254278ba8b87085cb5801aee4b7f8dc
joeyaza <JAzadehdel@gmail.com>
2016-02-24 12:10:25 +0000

    all showing up including random part

--- js/controller.js
+++ js/controller.js
@@ -3,37 +3,50 @@ angular
   .controller('MainController', MainController);
 
 MainController.$inject = ['$http'];
-function MainController($http){
-  var self = this;
-  self.all = [];
-  dates = [];
+  function MainController($http){
+    var self = this;
+    self.all = [];
+    self.rand = "";
+    self.randL = "";
 
-  for ( i = 0; i < 28; i++ ) {
-   var randNum = [];
-   var a = Math.floor(Math.random() * 11) + 5;
- }
- console.log(randNum)
+    for (i = 0; i <= 5; i++) {
+      $http.get("http://wxdata.weather.com/wxdata/weather/local/UKXX0085?CC=*&unit=m&dayf="+i+"",
+      {
+        transformResponse: function (cnv) {
+          var x2js = new X2JS();
+          var aftCnv = x2js.xml_str2json(cnv);
+          return aftCnv;
+        }
+      })
+      .success(function (response) {
+        self.all = response;
+      });
+  }
+
+  function randHigh() {
+    var arr = [];
+    for (i = 0; arr.length < 29; i++) {
+     i = Math.floor(Math.random() * 11 | 0) + 15
+     arr.push(i);
+   }
+   var sum = arr.reduce(function(a, b) { return a + b; }, 0);
+   self.rand = Math.floor(sum/arr.length);
+  }
+  randHigh();
+
+  function randLow() {
+    var arr = [];
+    for (i = 0; arr.length < 29; i++) {
+     i = Math.floor(Math.random() * 11 | 0) + 5
+     arr.push(i);
+   }
+   var sum = arr.reduce(function(a, b) { return a + b; }, 0);
+   self.randL = Math.floor(sum/arr.length);
+  }
+  randLow();
 
 
-var b = Math.floor(Math.random() * 11) + 5;
-
-var a = Math.floor(Math.random() * 15) + 11; 
-
-console.log(a)
-console.log(b)
+};
 
 
-  for (i = 0; i <= 5; i++) {
-  $http.get("http://wxdata.weather.com/wxdata/weather/local/UKXX0085?CC=*&unit=m&dayf="+i+"",
-                      {
-                          transformResponse: function (cnv) {
-                              var x2js = new X2JS();
-                              var aftCnv = x2js.xml_str2json(cnv);
-                              return aftCnv;
-                          }
-                      })
-              .success(function (response) {
-                self.all = response;
-              });
-          }
-        };
+
